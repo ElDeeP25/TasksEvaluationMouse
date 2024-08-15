45cf424387c93877f11e79aa4cf62c5dd5b62bbf
ElDeeP25 <101210462+ElDeeP25@users.noreply.github.com>
2024-08-15 19:12:36 +0300

    Add project files.

---
+++ TasksEvaluation.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TasksEvaluation.Web.Helper;
+using TasksEvaluation.Web.Models;
+
+namespace TasksEvaluation.Web.Controllers
+{
+    public class HomeController : Controller
+        
+    {
+        private readonly IEmailSender _emailSender;
+
+       // private readonly ILogger<HomeController> _logger;
+
+        public HomeController(IEmailSender emailSender)
+        {
+           this._emailSender = emailSender; 
+        }
+        public async Task<IActionResult> Index()
+        {
+          //  var reciever = "cilark201@qumax.com";
+          //  var subject = "Tset";
+          //  var message = "Hello world";
+          //  await _emailSender.SendEmailAsync(reciever, subject, message);
+            return View();
+        }
+       
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TasksEvaluation.Web/Helper/EmailSettings.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
+using System.Net;
+using System.Net.Mail;
+using TasksEvaluation.Infrastructure.Models;
+
+namespace TasksEvaluation.Web.Helper
+{
+    public class EmailSettings
+    {
+        public static void SendEmail(Email email)
+        {
+            /// 555 port mail server
+            ///naser@linkdv.com
+            /// 
+            var client = new SmtpClient("stemp.gmail.com", 587); 
+
+            //we can use pakage => mailkit : best
+
+           client.EnableSsl = true;
+ 
+           client.Credentials = new NetworkCredential("nassermessileo10@gmail.com", "password");
+
+           client.Send("nassermessileo10@gmail.com", email.To,email.subject,email.body);
+        
+        
+        
+        
+        }
+
+       // public  Task SendEmailAsync(string email, string subject, string message)
+       // {
+       //     var mail = "tutorialseu-dev@outlook.com";
+       //     var pw = "Test12345678!";
+       //     var client = new SmtpClient("stmp=mail.gmail.com,587")
+       //     {
+       //         EnableSsl = true,
+       //         Credentials = new NetworkCredential(mail, pw)
+       //     };
+       //  //   return client.SendAsync (new MailMessage(from: mail, to: email, subject, message));
+       // }
+    }
+}
